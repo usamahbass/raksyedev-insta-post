49e749974098461c7a158dc81f70c4e81ffc3455
usamahbass <basalamahusamah7@gmail.com>
2020-09-14 20:22:44 +0700

    simple-svelte-tabs

---
+++ svelte-tabs/src/App.svelte
@@ -0,0 +1,19 @@
+<!-- App.svelte -->
+
+<script>
+  import Tabs from "./components/Tabs.svelte";
+
+  let tabsItems = ["Home", "About"];
+  let tabsActive = "Home";
+
+  const tabChange = (e) => (tabsActive = e.detail);
+</script>
+
+<main>
+  <Tabs {tabsItems} {tabsActive} on:tabChange={tabChange} />
+  {#if tabsActive === 'Home'}
+    <h1>Home Page</h1>
+  {:else if tabsActive === 'About'}
+    <h1>About Page</h1>
+  {/if}
+</main>
---
+++ svelte-tabs/src/components/Tabs.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  let dispatch = createEventDispatcher();
+  export let tabsItems;
+  export let tabsActive;
+</script>
+
+<!-- Tabs.svelte -->
+
+<style>
+  .tabs {
+    margin-bottom: 40px;
+  }
+  ul {
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    list-style: none;
+  }
+  li {
+    margin: 0 16px;
+    font-size: 18px;
+    color: #555555;
+    cursor: pointer;
+  }
+  .active {
+    color: #d91b42;
+    border-bottom: 2px solid #d91b42;
+    padding-bottom: 8px;
+  }
+</style>
+
+<!-- Tabs.svelte -->
+
+<div class="tabs">
+  <ul>
+    {#each tabsItems as tabsItem}
+      <li on:click={() => dispatch('tabChange', tabsItem)}>
+        <div class:active={tabsItem === tabsActive}>{tabsItem}</div>
+      </li>
+    {/each}
+  </ul>
+</div>
